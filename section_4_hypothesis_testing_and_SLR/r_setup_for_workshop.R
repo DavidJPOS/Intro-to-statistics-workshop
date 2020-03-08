9d1d2dc8818986f62bbfd5fcbb4702a9ac7eeb72
DavidJPOS <davidjposullivan@gmail.com>
2020-03-08 20:08:50 +0000

    set up files

---
+++ section_4_hypothesis_testing_and_SLR/r_setup_for_workshop.R
@@ -0,0 +1,12 @@
+##################################################
+## Project: workshop
+## Script purpose: install packages required for work shop
+## Date: 03-03-2020
+## Author: David JPO'Sullivan
+##################################################
+
+install.packages('tidyverse')
+install.packages('broom')
+install.packages('flex')
+install.packages("flexdashboard")
+install.packages('knitr')
